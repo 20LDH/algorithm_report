1749d44ae2149e237bce013709ef2976839e0d81
PC <PC@DESKTOP-DRANT0V>
2023-03-31 22:27:57 +0900

    최대숫자 구하기

---
+++ 최대숫자구하기/최대숫자구하기/최대숫자구하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int get_max_card(int cards[], int size) {
+    int max = cards[0];
+    for (int i = 1; i < size; i++) {
+        if (cards[i] > max) {
+            max = cards[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int cards[] = { 3, 8, 4, 1, 9, 7, 5, 2, 6, 10 };
+    int size = sizeof(cards) / sizeof(cards[0]);
+    int max_card = get_max_card(cards, size);
+    printf("The maximum card is %d\n", max_card);
+    return 0;
+}
